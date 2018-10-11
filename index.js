1c96d9680db5633a4bd4d19059d1e73a4c089724
Anthony Dz <doubbz@gmail.com>
2018-10-11 12:14:00 +0200

    fetch for bulbasaur instead

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const axios = require('axios');
 
 exports.pokeapi = (request, response) => {
-  axios.get('https://pokeapi.co/api/v2/pokemon/')
+  axios.get('https://pokeapi.co/api/v2/pokemon/1/')
   .then(function (res) {
     console.log(res.data);
     response.status(200).send(res.data);
