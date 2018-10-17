f54728280edf0082417ceae2f2e4c0627c64c76f
antho_doubbz <adziubecki@campings.com>
2018-10-17 14:19:08 +0200

    add new function

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const axios = require('axios')
 exports.pokeapi = (request, response) => {
   axios.get('https://pokeapi.co/api/v2/pokemon/1/')
     .then(function (res) {
-      console.log(res.data)
       response.status(200).send(res.data)
     })
+}
+
+exports.helloworld = (request, response) => {
+  response.status(200).send('Hello World!')
 }
