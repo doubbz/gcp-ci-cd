84f83035e9f8285d7aacdb35a37e9eb7f078939a
antho_doubbz <adziubecki@campings.com>
2018-10-17 14:55:17 +0200

    wartortle

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const axios = require('axios')
 
 exports.pokeapi = (request, response) => {
-  axios.get('https://pokeapi.co/api/v2/pokemon/1/')
+  axios.get('https://pokeapi.co/api/v2/pokemon/8/')
     .then(function (res) {
       response.status(200).send(res.data)
     })
